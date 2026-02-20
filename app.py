ab96a322b80c7460cefc11151e83bf4ad2c73688
João Victor <gzk2fntmbz@privaterelay.appleid.com>
2026-02-20 02:08:56 -0300

    Update app run configuration to use environment port

--- app.py
+++ app.py
@@ -64,7 +64,6 @@ def remover(id):
     salvar_tarefas(tarefas)
     return redirect("/")
 
-
-# ----------------- run -----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
